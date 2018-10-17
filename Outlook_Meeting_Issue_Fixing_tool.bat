99cf4c5470909704df415f7efd67e64f5173c7c0
Bharat Babbar <bharatbabbar28@gmail.com>
2018-10-17 20:14:30 +0530

    Update Outlook_Meeting_Issue_Fixing_tool.bat

--- Outlook_Meeting_Issue_Fixing_tool.bat
+++ Outlook_Meeting_Issue_Fixing_tool.bat
@@ -18,7 +18,7 @@ cls
 	:Title
 color 0E
 ECHO  ************************************************************																								   
-ECHO    "Either there is no default mail client or current mail client cannot fulfill the messaging request.
+ECHO    "Either there is no default mail client or current mail client cannot fulfill the messaging request. 
 ECHO	   Please run Microsoft Outlook and set it as the default mail client."
 ECHO	   This script will fix meeting issue in your MS Outlook         
 
@@ -33,10 +33,28 @@ color F1
 rmdir  "c:\acl" /Q /S
 md c:\acl
 cd c:\acl
-curl https://download.microsoft.com/download/1/7/d/17d82b72-bc6a-4dc8-bfaa-98b37b22b367/subinacl.msi -O
+bitsadmin /transfer "Download zip" /download /priority normal https://download.microsoft.com/download/1/7/d/17d82b72-bc6a-4dc8-bfaa-98b37b22b367/subinacl.msi "c:\acl\subinacl.msi"
 
 
-:installation
+color F1
+@ECHO off
+ECHO  Pleae select one of the options
+ECHO  ----------------------------------------------------------------------------------------------------------------------
+ECHO  1 For Windows 32 bit Operating System
+ECHO  2 For Windows 64 bit Operating System
+ECHO  Q Quit (task will not be done if you choose this option)
+ECHO  ----------------------------------------------------------------------------------------------------------------------
+
+
+
+SET /p option=Please enter one of the options:
+
+if %option%==1  ( goto :WindowsX86 )		else set /a er=1
+if %option%==2  ( goto :WindowsX64 )		else set /a er=er+1
+if %option%==Q  ( goto :ForExit )			else set /a er=er+1
+
+
+:WindowsX86
 color F1
 @echo off
 
@@ -44,14 +62,22 @@ msiexec  /i subinacl.msi /qn
 echo "Intalled press any key to proceed permission"
 echo off
 "C:\Program Files (x86)\Windows Resource Kits\Tools\SUBINACL.exe" /keyreg "HKEY_CLASSES_ROOT\Installer\Components\F1291BD604B860441AB89E60BDEE0F9C" /setowner=administrators /grant=everyone=f
-goto end
+goto :ForExit
 
 
-@pause
+:WindowsX64
+color F1
+@echo off
+
+msiexec  /i subinacl.msi /qn
+echo "Intalled press any key to proceed permission"
+echo off
 "C:\Program Files\Windows Resource Kits\Tools\SUBINACL.exe" /keyreg "HKEY_CLASSES_ROOT\Installer\Components\F1291BD604B860441AB89E60BDEE0F9C" /setowner=administrators /grant=everyone=f
-goto end
+goto :ForExit
 
-:end
+
+
+:ForExit
 cd c:\
 rmdir  "c:\acl" /Q /S
 echo.
